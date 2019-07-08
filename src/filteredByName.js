e5b5596e4a8db51c461cfe9006328bc290b4b1ba
sofiamejiamuro <sofiamejiamuro@gmail.com>
2019-07-08 11:10:58 -0500

    muestrra next evoluvion en funcion busqueda por nombre

--- src/filteredByName.js
+++ src/filteredByName.js
@@ -5,10 +5,9 @@ const createNodes = (place) => {
   
   
    let keysExtraction = Object.keys(place[0])
-   console.log(keysExtraction);
-   
    let valuesExtraction = Object.values(place[0])
-   console.log(valuesExtraction);
+  console.log(valuesExtraction);
+  
 
    var searchPokemonCard = document.createElement("div");
    var placeArray = ["name","img","type","height","weight","weaknesses","candy_count","next_evolution"]
@@ -27,53 +26,53 @@ const createNodes = (place) => {
             searchPokemonCard.appendChild(element);
 
             document.getElementById("showFilteredPokemons").appendChild(searchPokemonCard);    
-                     
-/*REVISAR YA ESTA LEYENDO PERO QUÉ ESTA HACIENDO?? */ 
-                    }else if (keysExtraction[i]=== placeArray[7]) {
 
-                      var keysNextEvo = Object.keys(placeArray[7]);
-                      var valuesNextEvo = Object.values(placeArray[7]);
-                      
-                      for (let i = 0; i < keysNextEvo.length; i++) {
-
-                        var element = document.createElement("p");
-                        element.setAttribute("class",keysNextEvo[i]);
-                                            
-                        var content = document.createTextNode(keysNextEvo[i]    +  ": "  +
-                        valuesNextEvo[i]);          
-                        element.appendChild(content); 
-                        searchPokemonCard.appendChild(element);
-
-                        document.getElementById("showFilteredPokemons").appendChild(searchPokemonCard);
-
-                      }
-                      
-          /*funciona*/
-                    }  else if (keysExtraction[i]=== placeArray[0] || 
-                               keysExtraction[i]=== placeArray[2] || 
-                               keysExtraction[i]=== placeArray[3] ||
-                               keysExtraction[i]=== placeArray[4] ||
-                               keysExtraction[i]=== placeArray[5] ||
-                               keysExtraction[i]=== placeArray[6] )
-                               
+                  }  else if   (keysExtraction[i]=== placeArray[7] )
+                                                          
                                {                     
                       var element = document.createElement("p");
                       element.setAttribute("class",keysExtraction[i]);
                                             
                       var content = document.createTextNode(keysExtraction[i]    +  ": "  +
-                      valuesExtraction[i]);          
+                      valuesExtraction[i][0].name);          
             element.appendChild(content); 
             searchPokemonCard.appendChild(element);
+                      
+                      var element2 = document.createElement("p");
+                      element2.setAttribute("class",keysExtraction[i]);
+                      var content2 = document.createTextNode(keysExtraction[i]    +  ": "  +
+                      valuesExtraction[i][1].name);          
+            element2.appendChild(content2); 
+            searchPokemonCard.appendChild(element2);
+
 
             document.getElementById("showFilteredPokemons").appendChild(searchPokemonCard);    
-                  } 
+            console.log(valuesExtraction[i][0].name);
+            
+                  } else if  (  keysExtraction[i]=== placeArray[2] || 
+                                keysExtraction[i]=== placeArray[3] ||
+                                keysExtraction[i]=== placeArray[4] ||
+                                keysExtraction[i]=== placeArray[5] ||
+                                keysExtraction[i]=== placeArray[6] )
+                  { 
+                    var element = document.createElement("p");
+                    element.setAttribute("class",keysExtraction[i]);
+                                          
+                    var content = document.createTextNode(keysExtraction[i]    +  ": "  +
+                    valuesExtraction[i]);          
+          element.appendChild(content); 
+          searchPokemonCard.appendChild(element);
+                  }
              
       
    }
  
-
-
  }
+
+
+
+
+
  
  
  const showPokemonFiltered = () => {
